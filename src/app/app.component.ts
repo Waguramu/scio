315d7653dd4c1d469aebfaf4fc7e5c72ef8b1401
Wagram Airiian <vagram.airiian@tum.de>
2019-04-12 11:15:08 +0200

    Rerouted components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,10 +19,4 @@ export class AppComponent {
     ) {
         this.authenticationService.currentUser.subscribe(x => this.currentUser = x);
     }
-
-    logout() {
-        this.authenticationService.logout();
-        this.router.navigate(['/login']);
-    }
-
 }
---
+++ src/app/email/email.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    templateUrl: 'email.component.html',
+})
+
+export class EmailComponent {
+
+    constructor(){};
+}
