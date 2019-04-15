917f2732ae92d499e634991cf8c52648f2abce7e
Vinpasso <vincent@jd-bode.de>
2019-04-15 13:28:24 +0200

    Add interceptor log messages

--- src/app/_helpers/jwt.interceptor.ts
+++ src/app/_helpers/jwt.interceptor.ts
@@ -1,14 +1,16 @@
-import { Injectable } from '@angular/core';
-import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import {Injectable} from '@angular/core';
+import {HttpRequest, HttpHandler, HttpEvent, HttpInterceptor} from '@angular/common/http';
+import {Observable} from 'rxjs';
 
-import { AuthenticationService } from '@/_services';
+import {AuthenticationService} from '@/_services';
 
 @Injectable()
 export class JwtInterceptor implements HttpInterceptor {
-    constructor(private authenticationService: AuthenticationService) { }
+    constructor(private authenticationService: AuthenticationService) {
+    }
 
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        console.log("Authentication interceptor: Intercepted request " + request.urlWithParams + " current user " + this.authenticationService.currentUser);
         // add authorization header with jwt token if available
         let currentUser = this.authenticationService.currentUserValue;
         if (currentUser && currentUser.token) {
@@ -17,6 +19,7 @@ export class JwtInterceptor implements HttpInterceptor {
                     Authorization: `Bearer ${currentUser.token}`
                 }
             });
+            console.log("Authentication interceptor: Added auth token " + currentUser.token + ".");
         }
 
         return next.handle(request);
